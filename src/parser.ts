6724bcdd327565894de8c6346e02a70b5b182e92
jiahua <jiahua.xu@outlook.com>
2023-10-24 22:21:06 +0800

    refactor: pass build

--- src/parser.ts
+++ src/parser.ts
@@ -4,12 +4,12 @@
 export type Parser = (html: string, contextLocalName?: string) => string;
 export let parse: Parser = (_html) => {
   console.error("Error: deno-dom: No parser registered");
-  Deno.exit(1);
+  return "";
 };
 
 export let parseFrag: Parser = (_html, _contextLocalName) => {
   console.error("Error: deno-dom: No parser registered");
-  Deno.exit(1);
+  return "";
 };
 
 const originalParse = parse;
